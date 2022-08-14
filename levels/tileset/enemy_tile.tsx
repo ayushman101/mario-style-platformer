ec98e9da841da051dc1e0907983ea49b94e86e8c
ayushman101 <87219658+ayushman101@users.noreply.github.com>
2022-08-14 15:41:42 +0530

    Add files via upload

---
+++ levels/tileset/enemy_tile.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="enemy_tile" tilewidth="51" tileheight="46" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="51" height="46" source="../../graphics/enemy/run/3.png"/>
+ </tile>
+</tileset>
